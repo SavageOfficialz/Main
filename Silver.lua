36256929b59c59ae85223970d5f9986fc85438e5
SavageOfficialz <savagehubofficial@gmail.com>
2025-01-11 23:45:29 +0700

    Create Silver.lua

---
+++ Silver.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 95295765150201 then
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/gkn8odey7rzs13cohps0ydwmjcnkdblj.lua"))()
+else
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/1mpmz9g32i4uf2upjni185o00q5orvo2.lua"))()
+end
